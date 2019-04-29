5626bac831686c99c17fc7e17022a17cd618e50b
Crowdbotics <team@crowdbotics.com>
2019-04-29 13:44:14 +0000

    initial commit

---
+++ android/app/src/main/java/com/civics_economic_revi_2849/MainActivity.java
@@ -0,0 +1,15 @@
+package com.civics_economic_revi_2849;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "civics_economic_revi_2849";
+    }
+}
